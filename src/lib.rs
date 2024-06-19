dba4ba0df4ca86bd188f208fb2c7411a5b4d14e8
G2-Games <ke0bhogsg@gmail.com>
2024-06-19 16:40:32 -0500

    Fixed doc failure

--- src/lib.rs
+++ src/lib.rs
@@ -7,22 +7,24 @@
 //! To use this library, first you need to get a device from [`cross_usb`] and
 //! then open a [`NetMDContext`].
 //!
-//! ```rust
-//! use cross_usb::prelude::get_device;
+//! ```no_run
+//! # tokio_test::block_on(async {
+//! use cross_usb::get_device;
 //! use minidisc::netmd::base::DEVICE_IDS_CROSSUSB;
 //! use minidisc::NetMDContext;
 //!
 //! // Get a device using the built-in list of descriptors for minidisc devices
-//! let dev_descriptor = cross_usb::get_device(DEVICE_IDS_CROSSUSB).await
+//! let dev_descriptor = cross_usb::get_device(DEVICE_IDS_CROSSUSB.to_vec()).await
 //!     .expect("Failed to find device");
 //!
 //! // Open a NetMD Context with the device
-//! let context = NetMDContext::new(dev_descriptor).await
+//! let mut context = NetMDContext::new(dev_descriptor).await
 //!     .expect("Could not create context");
 //!
 //! // Perform operations on it ...
 //! context.list_content().await
 //!     .expect("Could not list disc contents");
+//! # })
 //! ```
 
 pub mod netmd;
